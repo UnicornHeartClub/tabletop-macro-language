08a27bc261bcdf0993c44704f4ab6f2dbabf9f76
Thomas Lackemann <tommylackemann@gmail.com>
2017-10-30 15:45:17 -0400

    WIP can parse a simple macro

--- src/parser.rs
+++ src/parser.rs
@@ -7,6 +7,7 @@ use std::str;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Program {
+    name: MacroOp,
     steps: Vec<Step>,
 }
 
@@ -31,6 +32,7 @@ enum MacroOp {
 #[derive(Debug, PartialEq, Eq)]
 enum RollOp {
     Advantage,
+    Comment,
     Disadvantage,
     NdD,
 }
@@ -41,21 +43,31 @@ enum SayOp {
     To,
 }
 
+/// Matches a command
 named!(command <&[u8], MacroOp>, alt!(
-    map!(tag!("!roll"),     |_| MacroOp::Roll)      |
-    map!(tag!("!r"),        |_| MacroOp::Roll)      |
-    map!(tag!("!say"),      |_| MacroOp::Say)       |
-    map!(tag!("!s"),        |_| MacroOp::Say)       |
-    map!(tag!("!whisper"),  |_| MacroOp::Whisper)   |
-    map!(tag!("!w"),        |_| MacroOp::Whisper)
+    map!(ws!(tag!("!roll")),     |_| MacroOp::Roll)      |
+    map!(ws!(tag!("!r")),        |_| MacroOp::Roll)      |
+    map!(ws!(tag!("!say")),      |_| MacroOp::Say)       |
+    map!(ws!(tag!("!s")),        |_| MacroOp::Say)       |
+    map!(ws!(tag!("!whisper")),  |_| MacroOp::Whisper)   |
+    map!(ws!(tag!("!w")),        |_| MacroOp::Whisper)
 ));
 
-named!(name <&[u8], MacroOp>, do_parse!(
+/// Matches a macro name
+named!(name <&[u8], MacroOp>, ws!(do_parse!(
     tag!("#") >>
     name: map_res!(is_not!(" \t\r\n"), |r: &[u8]| String::from_utf8(r.to_vec())) >>
     (MacroOp::Name(name))
+)));
+
+/// Matches any type of operation
+named!(op <&[u8], MacroOp>, alt!(
+    name |
+    command |
+    ws!(primitive)
 ));
 
+/// Matches primitive operations
 named!(primitive <&[u8], MacroOp>, alt!(
     map!(tag!("*"), |_| MacroOp::Multiply)  |
     map!(tag!("+"), |_| MacroOp::Add)       |
@@ -63,9 +75,55 @@ named!(primitive <&[u8], MacroOp>, alt!(
     map!(tag!("/"), |_| MacroOp::Divide)
 ));
 
+/// Parse the complete macro
+named!(parse <&[u8], Program>, do_parse!(
+    prog_name: name >>
+    op_type: op >>
+    args: ws!(many0!(string)) >>
+    (Program {
+        name: prog_name,
+        steps: vec![Step {
+            op: op_type,
+            args,
+        }],
+    })
+));
+
+/// Match alphanumeric values to strings
+named!(string <&[u8], String>,
+    do_parse!(
+        word: ws!(alphanumeric) >>
+        (String::from_utf8(word.to_vec()).unwrap())
+    )
+);
+
+// @todo Match delimited values between " and ' to strings
 
 #[test]
-fn test_name() {
+fn test_parser() {
+    let program = Program {
+        name: MacroOp::Name(String::from("macro-name")),
+        steps: vec![Step {
+            op: MacroOp::Roll,
+            args: vec![ "1d20".to_string() ],
+        }],
+    };
+    let (_, result) = parse(b"#macro-name !roll 1d20").unwrap();
+    assert_eq!(result, program);
+
+    // let program = Program {
+        // name: MacroOp::Name(String::from("macro-name-2")),
+        // steps: vec![Step {
+            // op: MacroOp::Say,
+            // args: vec![ "\"Hello, world!\"".to_string() ],
+        // }],
+    // };
+    // let (_, result) = parse(b"#macro-name-2 !say \"Hello, world!\"").unwrap();
+    // assert_eq!(result, program);
+}
+
+#[test]
+fn test_name_parser() {
     let (_, result) = name(b"#macro_name").unwrap();
     assert_eq!(result, MacroOp::Name(String::from("macro_name")));
 
@@ -78,9 +136,40 @@ fn test_name() {
     let (_, result) = name(b"#Z123macro-name").unwrap();
     assert_eq!(result, MacroOp::Name(String::from("Z123macro-name")));
 
+    let (_, result) = name(b"#macro-name cannot have spaces").unwrap();
+    assert_eq!(result, MacroOp::Name(String::from("macro-name")));
+
     let bad_result = name(b"macro_name");
     match bad_result {
         IResult::Error(e) => assert_eq!(e, ErrorKind::Tag),
         _ => assert_eq!(1, 0),
     }
+}
+
+#[test]
+fn test_command_parser_roll() {
+    let (_, result) = command(b"!roll 1d20").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"!r 1d20").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"!roll advantage").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"!roll adv").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"!roll disadvantage").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"!roll dis").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+}
+
+#[test]
+fn test_op_parser() {
+    let (_, result) = op(b"    #test-macro   ").unwrap();
+    assert_eq!(result, MacroOp::Name(String::from("test-macro")));
+    let (_, result) = command(b"    !roll 1d20 ").unwrap();
+    assert_eq!(result, MacroOp::Roll);
+    let (_, result) = command(b"   !say \"Hello!\" ").unwrap();
+    assert_eq!(result, MacroOp::Say);
+    let (_, result) = command(b"   !whisper").unwrap();
+    assert_eq!(result, MacroOp::Whisper);
 }

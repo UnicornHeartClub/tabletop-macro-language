819f34c1facdf06b3b823a590ee96b1c8cf82084
Thomas Lackemann <tommylackemann@gmail.com>
2017-11-10 06:56:36 -0500

    invoke a prompt from js and return in rust

--- src/dom.rs
+++ src/dom.rs
@@ -1,10 +1,23 @@
 use std::ffi::CString;
+use std::ffi::CStr;
+
+/// Safe wrapper for our JS function `prompt`.
+pub fn prompt(x: &str) -> String {
+    let x = CString::new(x).unwrap();
+    let ptr = x.as_ptr();
+    let data = unsafe {
+        CStr::from_ptr(ffi::prompt(ptr))
+    };
+    data.to_string_lossy().into_owned()
+}
 
 /// Evaluate JavaScript
 pub fn eval(x: &str) -> i32 {
     let x = CString::new(x).unwrap();
     let ptr = x.as_ptr();
-    unsafe { ffi::emscripten_run_script_int(ptr) }
+    unsafe {
+        ffi::emscripten_run_script_int(ptr)
+    }
 }
 
 // Lovingly adopted from https://github.com/kainino0x/wasm-call-js-from-rust
@@ -12,6 +25,8 @@ mod ffi {
     use libc::*;
 
     extern "C" {
+        // This extern is defined in `html/library.js`.
+        pub fn prompt(x: *const c_char) -> *mut c_char;
         // This extern is built in by Emscripten.
         pub fn emscripten_run_script_int(x: *const c_char) -> c_int;
     }

9ced4e418411573fded5295a41d92e7edf579049
Thomas Lackemann <tommylackemann@gmail.com>
2018-01-08 15:55:12 -0500

    removes all execution from the application

--- src/token.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-use arg::ArgValue;
-
-#[derive(Debug, Clone, Deserialize, PartialEq, Serialize)]
-pub struct Token {
-    pub attributes: HashMap<String, ArgValue>,
-    pub macros: HashMap<String, ArgValue>,
-}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
  module.exports = {
-   entry: `${__dirname}/index.js`,
+   entry: ['babel-polyfill', `${__dirname}/index.js` ],
    output: {
      filename: 'bundle.js',
      libraryTarget: 'commonjs2',
@@ -13,25 +13,12 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
    module: {
      rules: [
        {
-         test: /\.rs$/,
-         use: {
-           loader: 'rust-wasm-loader',
-           options: {
-             args: '--features web',
-             nightly: true,
-             path: 'dist',
-             release: true,
-             wasmBinaryFile: '/macro.wasm',
-           },
-         },
-       },
-       {
          test: /\.js$/,
          exclude: /(node_modules)/,
          use: {
            loader: 'babel-loader',
            options: {
-             presets: ['env'],
+             presets: ['@babel/preset-env'],
            },
          },
        },

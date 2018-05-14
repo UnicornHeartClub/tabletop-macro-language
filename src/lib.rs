5f6c442fcc326a4aa6c3bdebc1ada5a5014cb86d
Thomas Lackemann <tommylackemann@gmail.com>
2018-05-14 17:02:19 -0400

    alpha return parser error

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,13 @@ pub mod output;
 pub mod parser;
 pub mod step;
 
-#[derive(Serialize, Deserialize)]
+use nom::Err::Error;
+use nom::simple_errors::Context::Code;
+
+#[derive(Debug, Serialize, Deserialize)]
 struct OutputError {
     error: String,
+    context: String,
 }
 
 use std::mem;
@@ -46,27 +50,40 @@ pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
 /// Run input and return a typed array for use in javascript
 #[no_mangle]
 pub extern "C" fn parse(raw_input: *mut c_char) -> *mut c_char {
-    // Parse the input
+    // covert the input
     let input = safe_string(raw_input);
 
-    let prog = parse_p(input.as_slice());
+    // run the parser
+    let json = run(input);
 
-    // Parse the macro
+    // return the result
+    CString::new(json).unwrap().into_raw()
+}
+
+/// Run the internal parser and return a program or error
+fn run (input: Vec<u8>) -> String {
+    let prog = parse_p(input.as_slice());
     if prog.is_err() {
         // Push the error
         let error = prog.unwrap_err();
-        let output_error = OutputError {
-            error: format!("{:?}", error),
-        };
-        let json = serde_json::to_string(&output_error).unwrap();
 
-        CString::new(json).unwrap().into_raw()
+        let mut output_error = OutputError {
+            error: format!("{:?}", error),
+            context: "".to_string(),
+        };
+
+        if let Error(Code(list, _code)) = error {
+            let context = String::from_utf8(list.to_vec()).unwrap();
+            output_error.context = context;
+        }
+
+        // return error JSON
+        serde_json::to_string(&output_error).unwrap()
     } else {
         let (_, program) = prog.unwrap();
 
-        // Return as JSON
-        let json = serde_json::to_string(&program).unwrap();
-        CString::new(json).unwrap().into_raw()
+        // return success JSON
+        serde_json::to_string(&program).unwrap()
     }
 }
 
@@ -74,4 +91,21 @@ fn safe_string(input: *mut c_char) -> Vec<u8> {
     unsafe {
         CStr::from_ptr(input).to_bytes().to_owned()
     }
+}
+
+#[test]
+fn test_error_handling() {
+    // missing ending single quotation
+    let input: Vec<u8> = "#test !prompt test [ok, not 'ok]".as_bytes().to_vec();
+    let result = run(input);
+
+    let json: OutputError = serde_json::from_str(&result).unwrap();
+    assert_eq!(json.context, "[ok, not 'ok]".to_string());
+
+    // missing prompt input
+    let input: Vec<u8> = "#test !prompt [test, tester, testest]".as_bytes().to_vec();
+    let result = run(input);
+
+    let json: OutputError = serde_json::from_str(&result).unwrap();
+    assert_eq!(json.context, "[test, tester, testest]".to_string());
 }
